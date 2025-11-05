048c6b73f358e89705908178cad1327d1a946e25
Naman Kukreti <kukretinaman@gmail.com>
2025-11-05 14:57:39 +0530

    setup file created

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="MedChat",
+    version="0.0.0",
+    author="Naman Kukreti",
+    author_email="kukretinaman@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
---
+++ templates.py
@@ -0,0 +1,9 @@
+import os
+from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+list_of_files = [
+    
+]
